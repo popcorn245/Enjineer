f74f51633c263353ba9881078c36c7ce11a461c9
Bobby Johnson <b.johnson@metrogistics.com>
2014-02-11 16:47:38 -0600

    Setup for mobile layout and add animation to rocket.

--- js/enjineer.js
+++ js/enjineer.js
@@ -232,7 +232,27 @@ angular.module("ui.bootstrap",["ui.bootstrap.tpls","ui.bootstrap.transition","ui
 };
 
 var	app = angular.module("app",["ngRoute","ui.bootstrap"])
+		.controller('appCtrl', function($scope, $http){
+			$scope.menu = function(){
+				$sidebar = $e("#sidebar"); 
+				if($sidebar.css("display") == 'none'){
+					$sidebar.css("display","block");
+				}else{
+					$sidebar.css("display","none");
+				}
+			};
+		})
 		.controller('homeCtrl', function($scope, $http){
+		
+		})
+		.controller('phpCtrl', function($scope, $http){
+		
+		})
+		.controller('jsCtrl', function($scope, $http){
+
+		})
+		.controller('addCtrl', function($scope, $http){
+
 		})
 		.config(['$routeProvider',
 	  		function($routeProvider) {
@@ -240,6 +260,18 @@ var	app = angular.module("app",["ngRoute","ui.bootstrap"])
 			    	.when('/home', {
 				        templateUrl: 'html/home.html',
 				        controller: 'homeCtrl'
+				    })
+				    .when('/add', {
+				        templateUrl: 'html/add.html',
+				        controller: 'Ctrl'
+				    })
+				    .when('/js', {
+				        templateUrl: 'html/js.html',
+				        controller: 'jsCtrl'
+				    })
+				    .when('/php', {
+				        templateUrl: 'html/php.html',
+				        controller: 'phpCtrl'
 				    })
 				    .otherwise({
 				        redirectTo: '/home'
--- js/src/app.js
+++ js/src/app.js
@@ -8,7 +8,27 @@ var $e = function(sel, ctx){
 };
 
 var	app = angular.module("app",["ngRoute","ui.bootstrap"])
+		.controller('appCtrl', function($scope, $http){
+			$scope.menu = function(){
+				$sidebar = $e("#sidebar"); 
+				if($sidebar.css("display") == 'none'){
+					$sidebar.css("display","block");
+				}else{
+					$sidebar.css("display","none");
+				}
+			};
+		})
 		.controller('homeCtrl', function($scope, $http){
+		
+		})
+		.controller('phpCtrl', function($scope, $http){
+		
+		})
+		.controller('jsCtrl', function($scope, $http){
+
+		})
+		.controller('addCtrl', function($scope, $http){
+
 		})
 		.config(['$routeProvider',
 	  		function($routeProvider) {
@@ -16,6 +36,18 @@ var	app = angular.module("app",["ngRoute","ui.bootstrap"])
 			    	.when('/home', {
 				        templateUrl: 'html/home.html',
 				        controller: 'homeCtrl'
+				    })
+				    .when('/add', {
+				        templateUrl: 'html/add.html',
+				        controller: 'Ctrl'
+				    })
+				    .when('/js', {
+				        templateUrl: 'html/js.html',
+				        controller: 'jsCtrl'
+				    })
+				    .when('/php', {
+				        templateUrl: 'html/php.html',
+				        controller: 'phpCtrl'
 				    })
 				    .otherwise({
 				        redirectTo: '/home'
